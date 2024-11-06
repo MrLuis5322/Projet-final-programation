45b96189391bd9a00bf4de0087329ececfb71b4b
Donavan Sirois <dsirois667@gmail.com>
2024-11-06 09:36:59 -0500

    Ajustement joueur

--- joueur.py
+++ joueur.py
@@ -3,10 +3,8 @@ import numpy as np  # Importation de NumPy pour les calculs mathématiques
 class Joueur:  # Définition de la classe Joueur
     def __init__(self, accueil, obstacles):  
         self.accueil = accueil  # Référence à l'interface d'accueil
-        self.obstacles = self.accueil.get_obstacles()
-        # self.obstacles_instance = Obstcales() # Créer une instance de la classe Obstacles
-        # self.obstacles = self.obstacles_instance.generer_obstacles()  # Génération des obstacles
-
+        self.obstacles = self.accueil.get_obstacles() # Reference aux obstacles
+        self.reference = (0, 0) # Reference au dernier point etant ete cree, sert a ne pas cree un point sur le dernier point cree
         self.joueur_position = self.generer_position_valide_point()  # Position valide pour le joueur
         self.cible_position = self.generer_position_valide_point()  # Position valide pour la cible
 
@@ -14,10 +12,15 @@ class Joueur:  # Définition de la classe Joueur
         while True:  # Boucle infinie jusqu'à obtenir une position valide
             xpoint = np.random.randint(0, 340)  # Génération d'un x aléatoire
             ypoint = np.random.randint(0, 180) # Génération d'un y aléatoire
-            is_valide = True
+            is_valide = True # Création d'une variable pour vérifier si le point est valide
             for (xobstacle, yobstacle, robstacle) in self.obstacles: # Pour chaque obstacle
-                distance = np.sqrt((xpoint-xobstacle)**2 + (ypoint - yobstacle)**2) # On calcul la distance entre le point et l'obstacle
-                if distance < (robstacle + 10):
-                    is_valide = False
-            if is_valide : return (xpoint, ypoint)
+                odistance = np.sqrt((xpoint-xobstacle)**2 + (ypoint - yobstacle)**2) # On calcul la distance entre le point et l'obstacle
+                if odistance < (robstacle + 10): # Si la distance est plus petit que le rayon de l'obstacle et que le rayon du point
+                    is_valide = False # Le point n'est pas valide
+                pdistance = np.sqrt((xpoint-self.reference[0])**2 + (ypoint - self.reference[1])**2) # On calcul la distance entre le point et le point de reference
+                if pdistance < 10: # Si le point n'est pas asse eloigne du point de reference
+                    is_valide = False # Le point n'est pas valide
+            if is_valide : # Si le point est valide
+                self.reference = (xpoint, ypoint) # Le point de reference devient le point
+                return (xpoint, ypoint) # On le retourne
             

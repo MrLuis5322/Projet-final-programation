4d90c522c2bf325c0846df2d1f8c09e2bf85dc48
MrLuis5322 <luis.lamontagne@gmail.com>
2024-11-23 13:10:12 -0500

    ajout image jouer

--- accueil.py
+++ accueil.py
@@ -7,6 +7,7 @@ from joueur import Joueur
 from tir import Tir  
 from traitementCSV import Obstacles
 from matplotlib.patches import Circle
+import matplotlib.image as mpimg
 
 
 class Accueil(tk.Frame):  # Définition de la classe Accueil comme un frame tkinter
@@ -16,15 +17,13 @@ class Accueil(tk.Frame):  # Définition de la classe Accueil comme un frame tkin
     def __init__(self, master, res):  # Appel du constructeur
         super().__init__(master)  # Super permet d'appeler plusieurs inheritance
 
-        image = 
-
         self.res = res  # Facteur de résolution
         self.score = 0  # Score initial
         self.temps = 121  # Temps initial
 
         self.fig, self.ax = plt.subplots()  # Création de la figure et des axes pour le tracé
-        self.fig.set_facecolor('#363737')
-        self.ax.set_facecolor('#363737')
+        self.fig.set_facecolor('gray')
+        self.ax.set_facecolor('white')
 
         self.obstacles_instance = Obstacles()  # Instance de la classe Obstacles
         self.obstacles = self.obstacles_instance.generer_obstacles()  # Liste des obstacles
@@ -142,11 +141,19 @@ class Accueil(tk.Frame):  # Définition de la classe Accueil comme un frame tkin
         self.master.ajouter_log(f"Partie terminée avec {self.score} points")
 
     def plot_obstacles_and_goal(self):  # Tracé des obstacles et de la cible
+    # Charger l'image pour l'arrière-plan
+        background_image = mpimg.imread('landscapefromabove.jpg')  # Remplacez par le chemin de votre image
+
+    # Afficher l'image en arrière-plan
+        self.ax.imshow(background_image, extent=[0, 360, 0, 200], aspect='auto')
+
+    # Tracer les obstacles
         for obstacle in self.joueur.obstacles:
             x, y, r = obstacle
             circle = Circle((x, y), r, color='red', alpha=0.5)
             self.ax.add_patch(circle)
 
+    # Tracer la position du joueur
         self.ax.plot(
             self.joueur.joueur_position[0],
             self.joueur.joueur_position[1],
@@ -154,6 +161,8 @@ class Accueil(tk.Frame):  # Définition de la classe Accueil comme un frame tkin
             markersize=15 * self.res,
             label='Joueur',
         )
+
+    # Tracer la position de la cible
         self.ax.plot(
             self.joueur.cible_position[0],
             self.joueur.cible_position[1],
@@ -162,10 +171,14 @@ class Accueil(tk.Frame):  # Définition de la classe Accueil comme un frame tkin
             label='Cible',
         )
 
+    # Configurer les limites et l'aspect des axes
         self.ax.set_xlim(0, 360)
         self.ax.set_ylim(0, 200)
         self.ax.set_aspect('equal', 'box')
         self.ax.legend(prop={"size": 15 * self.res}, markerscale=0.6 * self.res)
+
+
+
     def update_score_ville(self):
         self.villes_touche +=1
 
